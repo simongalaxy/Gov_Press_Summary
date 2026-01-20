8d71acd43f183480ee14158dfb803b655b9bc012
simongalaxy <simongalaxy2011@gmail.com>
2026-01-20 13:05:11 +0800

    updated webcrawler.py

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
 from tools.logger import Logger
 from tools.webCrawler import WebCrawler
-from tools.SQLiteDatabase import sqliteHandler, News
+from tools.SQLiteDatabase import sqliteHandler
 from tools.DocumentProcessor import DocumentProcessor
 from tools.ChromaDBHandler import ChromaDBHandler
 # from tools.ContentSummarizer import ContentSummarizer
-# from tools.NewsSummarizer import NewsSummarizer
+
 
 import asyncio
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-import datetime
-import calendar
 
-def generate_urls(year: int, month: int) -> list[str]:
-    
-    num_days = calendar.monthrange(year, month)[1]
-    dates = [datetime.date(year, month, day).strftime("%Y%m%d") for day in range(1, num_days + 1)]
-    urls = [f"https://www.info.gov.hk/gia/general/{date[:-2]}/{date[-2:]}.htm" for date in dates]
-    
-    return urls
-    
-
+# main program.
 def main():
     
     # initiate classes.
@@ -33,40 +23,35 @@ def main():
     docProcessor = DocumentProcessor(logger=logger)
     chromaDBHandler = ChromaDBHandler(logger=logger)
     # summarizer = ContentSummarizer(logger=logger)
-    # summarizer = NewsSummarizer(logger=logger)
+
     
     # generate urls by dates.
     year = 2025
-    month = 11
-    urls = generate_urls(
-        year=year,
-        month=month
-    )
-    logger.info(f"Years: {year}, Month: {month}")
-    logger.info(f"Total No. of urls: {len(urls)}")
+    month = 2
+    # day = 13
     
     # crawl all press release.
-    results_list = asyncio.run(crawler.fetch_all_news(urls=urls))
+    news_list = asyncio.run(crawler.crawl_all_news_pages(year=year, month=month))
     
     # save results into News and then create splitted documents for text summarization and saving to chromadb and then sqlitedb.
 
-    for result in results_list:
-        news_entry = News(
-            news_id=result.url.split("/")[-1].replace(".htm", ""),
-            title=result.metadata["title"],
-            published_date=result.markdown.split("\n")[-4].split(", ", 1)[-1].strip(),
-            published_time=result.markdown.split("\n")[-3].split(" ")[-2],
-            source_url=result.url,
-            content=result.markdown
-        )
+    # for result in results_list:
+    #     news_entry = News(
+    #         news_id=result.url.split("/")[-1].replace(".htm", ""),
+    #         title=result.metadata["title"],
+    #         published_date=result.markdown.split("\n")[-4].split(", ", 1)[-1].strip(),
+    #         published_time=result.markdown.split("\n")[-3].split(" ")[-2],
+    #         source_url=result.url,
+    #         content=result.markdown
+    #     )
         
-        # save to chromadb.
-        splitted_dococuments = docProcessor.create_Documents_from_news(news=news_entry)
-        news_entry.total_chunks = len(splitted_dococuments)
-        chromaDBHandler.add_splits_to_db(documents=splitted_dococuments)
+    #     # save to chromadb.
+    #     splitted_dococuments = docProcessor.create_Documents_from_news(news=news_entry)
+    #     news_entry.total_chunks = len(splitted_dococuments)
+    #     chromaDBHandler.add_splits_to_db(documents=splitted_dococuments)
 
-        # save to sqlitedb.
-        sqlDBHandler.create_news(news_item=news_entry)
+    #     # save to sqlitedb.
+    #     sqlDBHandler.create_news(news_item=news_entry)
     
     return
         
--- tools/webCrawler.py
+++ tools/webCrawler.py
@@ -1,81 +1,105 @@
 from crawl4ai import AsyncWebCrawler, CrawlerRunConfig, CacheMode, BrowserConfig, MemoryAdaptiveDispatcher
-from crawl4ai.deep_crawling import BFSDeepCrawlStrategy
 from crawl4ai.content_scraping_strategy import LXMLWebScrapingStrategy
-from crawl4ai.deep_crawling.filters import URLPatternFilter, FilterChain
 
-from pprint import pformat
+import datetime
+import calendar
 import asyncio
-import os
+from pprint import pprint
 import re
-from dotenv import load_dotenv
-load_dotenv()
 
 
 class WebCrawler:
     def __init__(self, logger):
         self.logger = logger
-        self.url_filter = URLPatternFilter(patterns=[r"^https:\/\/www\.info\.gov\.hk\/gia\/general\/\d{6}\/\d{2}\/P\d+\.htm$"])
-        self.filter_chain = FilterChain(filters=[self.url_filter])
         self.browser_config = BrowserConfig(
             headless=True,
-            text_mode=True,
-            )
-        self.deep_crawl_strategy = BFSDeepCrawlStrategy(
-                max_depth=1,  # Reduced depth for faster crawling
-                include_external=False,
-                filter_chain=self.filter_chain
-                )
-        self.crawl_config = CrawlerRunConfig(
-            deep_crawl_strategy=self.deep_crawl_strategy,
+            text_mode=True
+        )
+        self.crawl_config_NewsPage = CrawlerRunConfig(
             scraping_strategy=LXMLWebScrapingStrategy(),
             exclude_all_images=True,
             exclude_social_media_domains=True,
             exclude_external_links=True,
-            target_elements=['div[id="PRHeadline"]', 'span[id="pressrelease"]'],
-            cache_mode=CacheMode.BYPASS
-            )
-        self.dispatcher = MemoryAdaptiveDispatcher(
-            memory_threshold_percent=75.0,
-            max_session_permit=20
+            # Use valid CSS attribute selectors for better compatibility
+            target_elements=['div[id="PRHealine"]', 'span[id="pressrelease"]'], # for pres release content.
+            cache_mode=CacheMode.BYPASS,
         )
-        self.logger.info(f"Class - {WebCrawler.__name__} initiated.")
+        self.crawl_config_DatePage = CrawlerRunConfig(
+            scraping_strategy=LXMLWebScrapingStrategy(),
+            exclude_all_images=True,
+            exclude_social_media_domains=True,
+            exclude_external_links=True,
+            cache_mode=CacheMode.BYPASS,
+        )
+        self.dispatcher = MemoryAdaptiveDispatcher(
+            memory_threshold_percent=70,
+            check_interval=1,
+            max_session_permit=3
+        )
+        
+        self.logger.info(f"{WebCrawler.__name__} initiated.")
 
-    
-    async def crawl_date_page(self, url: str):
+
+    async def crawl_news_pages(self, urls: list[str]):
         async with AsyncWebCrawler(config=self.browser_config) as crawler:
-            self.logger.info(f"{WebCrawler.__name__}: Start crawl year page - {url} ...")
-            results = await crawler.arun(
-                url=url,
-                config=self.crawl_config
+            news_results = await crawler.arun_many(
+                urls=urls, 
+                config=self.crawl_config_NewsPage,
+                dispatcher=self.dispatcher
             )
-            
-            return results
+        self.logger.info(f"Total {len(news_results)} press release pages crawled.")
+        for news in news_results:
+            self.logger.info(f"url: {news.url}")
+            self.logger.info("Press releaase: \n%s", news.markdown)
+            self.logger.info("-"*50)
+              
+        return news_results
     
     
-    async def fetch_all_news(self, urls: list[str]):
-        self.logger.info(f"{WebCrawler.__name__}: Start fetch all news.")
+    async def crawl_date_pages(self, urls: list[str]):
+        async with AsyncWebCrawler(config=self.browser_config) as crawler:
+            results = await crawler.arun_many(
+                urls=urls, 
+                config=self.crawl_config_DatePage,
+                dispatcher=self.dispatcher
+            )
         
-        tasks = [self.crawl_date_page(url=url) for url in urls]
-        results = await asyncio.gather(*tasks)     
-        self.logger.info(f"{WebCrawler.__name__}: Total No. of year urls fetched: {len(urls)}")
+        # extract the job page links.
+        news_links = []
+        for i, result in enumerate(results):
+            links = result.links.get("internal", [])
+            for link in links:
+                if re.search(pattern=r"P.*\.htm", string=link["href"]):
+                    news_links.append(link["href"])
+                    
+        self.logger.info(f"Total {len(news_links)} press release page links crawled from {len(urls)} date pages.")    
+         
+        return news_links
 
-        result_lists = self.consolidate_results(results=results)
+
+    def generate_urls(self, year: int, month: int) -> list[str]:
+        num_days = calendar.monthrange(year, month)[1]
+        dates = [datetime.date(year, month, day).strftime("%Y%m%d") for day in range(1, num_days + 1)]
+        urls = [f"https://www.info.gov.hk/gia/general/{date[:-2]}/{date[-2:]}.htm" for date in dates]
+        for i, url in enumerate(urls):
+            self.logger.info(f"{i}: {url}")
         
-        return result_lists
-    
-    
-    def consolidate_results(self, results):
-        results_lists = []
-        for result in results:
-            for item in result:
-                if re.search(r"P.*\.htm", item.url): # to only get the result crawled from press release where its url with pattern starting with "P" and ending with ".htm".
-                # if "press_" not in item.url:
-                    results_lists.append(item)
-                    self.logger.info(f"url: {item.url}")
-                    self.logger.info(f"Content: \n%s", item.markdown)
-                    self.logger.info("-"*50)
-                
-        self.logger.info(f"{WebCrawler.__name__}: Total No. of Press Release fetched: {len(results_lists)}.")    
+        return urls
+
+
+    def crawl_all_news_pages(self, year: int, month: int): 
+        # Generate search pages.
+        urls = self.generate_urls(
+            year=year, 
+            month=month
+        )
+
+        # crawl all links for job pages from search pages.
+        news_links = asyncio.run(self.crawl_date_pages(urls=urls))
         
-        return results_lists # remove the first page.
-    
+        # crawl all contents from each job pages.
+        news_results = asyncio.run(self.crawl_news_pages(urls=news_links))
+
+        return news_results
+        
+        
